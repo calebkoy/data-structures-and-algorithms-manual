6355a29acd5372f9032d0ea29931d3372913fd6f
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-14 16:11:26 +0100

    Implement binary search

---
+++ implementations/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+  public int search(int[] array, int element) {
+    if (array.length == 0) return -1; // Return early
+    int low = 0; 
+    int high = array.length - 1;
+    int mid;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      if (array[mid] < element) {
+        low = mid + 1;
+        continue;
+      } else if (array[mid] > element) {
+        high = mid - 1;        
+        continue;
+      } else {
+        return mid;
+      }
+    }
+    return -1; // Didn't find the element
+  }
+}
