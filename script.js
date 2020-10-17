f0fb6f8f470a49e21cbce08902655a52004f595f
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-17 21:42:54 +0100

    Add some WIP styles

--- script.js
+++ script.js
@@ -2,66 +2,20 @@ $(document).ready(function() {
   $('.copy-code-button').click(function() {
     let clickedButton = $(this);
     let clickedButtonText = clickedButton.text();
-    // let clipboard = new ClipboardJS(clickedButton[0]); // Q: Why doesn't this work? A: TBC
-    
     let clickedButtonId = clickedButton.attr('id');    
     let clipboard = new ClipboardJS('#' + clickedButtonId);
-    console.log('Created clipboard element');
     clipboard.on('success', function(e) {
-      console.log('In clipboard success method');
       e.clearSelection();
       clickedButton.text('Copied!');
       setTimeout(function() {
         clickedButton.text(clickedButtonText);
       }, 2000);
     });
-    clipboard.on('error', function(e) {
-      e.clearSelection();
-      clickedButton.text('Error!');
+    clipboard.on('error', function(e) {      
+      clickedButton.text('Error: press Ctrl + C to copy');
       setTimeout(function() {
         clickedButton.text(clickedButtonText);
       }, 2000);
     });
   });
 });
-
-// var buttons = document.querySelectorAll('.copy-code-button');
-// var clipboard = new ClipboardJS(buttons);
-
-// var buttons = document.querySelectorAll('.copy-code-button');
-// buttons.forEach(button => {
-//   button.on('click', function() {
-//     let clickedButton = this;
-//   })
-// })
-
-// var clipboard = new ClipboardJS('.copy-code-button', {
-//   target: function(trigger) {
-//     return trigger.parentNode.parentNode;
-//   }
-// });
-
-// clipboard.on('success', function(e) {
-//   // console.log(e);
-//   console.log("Successful copy!");  
-  
-//   // console.log('e.text: ', e.text);
-//   // e.text = 'Copied!';  
-
-//   console.log(clipboard.target());
-//   // clickedButton = clipboard.clickedButton;
-//   // clickedButton.innerText = 'Copied!';  
-//   e.clearSelection();
-//   // window.getSelection().removeAllRanges();
-//   // window.getSelection().addRange(document.createRange());
-// });
-
-// clipboard.on('error', function(e) {
-//   // console.log(e);
-//   console.log("Failed copy!");
-// });
-
-// clipboard = Navigation.clipboard;
-// function copyCode() {
-
-// }
