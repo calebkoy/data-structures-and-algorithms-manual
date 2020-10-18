86d7a27276116e2c791a10b9a91863a45e6ea36c
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-18 14:54:13 +0100

    Implement stack

---
+++ implementations/Stack.java
@@ -0,0 +1,31 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class Stack<E> {
+  private LinkedList<E> list;
+
+  public Stack() {}  
+  
+  public int size() {
+    return list.size();    
+  }
+
+  public boolean empty() {
+    return list.size() == 0;
+  }    
+  
+  public E push(E element) {
+    list.addLast(element);
+    return element;
+  }
+  
+  public E peek() {
+    if (empty()) throw new EmptyStackException();
+    return list.peekLast();
+  }
+  
+  public E pop() {
+    if (empty()) throw new EmptyStackException();
+    return list.removeLast();
+  }  
+}
