16e888679e4e11cb82d812a8e1e7d0cb46dfd40c
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-22 06:55:07 +0100

    Explicitly specify base url in head

--- setBaseUrl.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  // Explicitly set document base URL 
-  // to prevent incorrect routing on GitHub pages
-  let baseHref = "/data-structures-and-algorithms/";
-  $('base').attr("href", baseHref);
-});
